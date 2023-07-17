2bbcc1547dd88db19e995495bfd95dde3f0fddc7
winstonsmith1897 <m.simoni5@studenti.unipi.it>
2023-07-17 16:06:18 +0200

    Adjust kernel monitor code

--- send_data.py
+++ send_data.py
@@ -1,11 +1,12 @@
-import json
-import time
-import threading
-import websocket
 import datetime
 import hashlib
-import re
+import json
 import platform
+import re
+import threading
+import time
+
+import websocket
 
 
 def publish_thread(data):
@@ -16,17 +17,18 @@ def publish_thread(data):
 
     # Generate a random hash using SHA-256 algorithm
     hash_object = hashlib.sha256()
-    hash_object.update(bytes(str(now), 'utf-8'))
+    hash_object.update(bytes(str(now), "utf-8"))
     hash_value = hash_object.hexdigest()
 
     # Concatenate the time and the hash
     request_id = str(requestor_id) + str(now) + hash_value
-    request_id = re.sub('[^a-zA-Z0-9\n\.]', '', request_id).replace('\n', '').replace(' ', '')
+    request_id = (
+        re.sub("[^a-zA-Z0-9\n\.]", "", request_id).replace("\n", "").replace(" ", "")
+    )
 
-    ws = websocket.WebSocketApp("ws://localhost:3000/ws",
-                                on_open=on_open,
-                                on_error=on_error,
-                                on_close=on_close)
+    ws = websocket.WebSocketApp(
+        "ws://localhost:3000/ws", on_open=on_open, on_error=on_error, on_close=on_close
+    )
 
     def send_data():
         ws_req = {
@@ -39,8 +41,8 @@ def publish_thread(data):
                     "request_id": str(request_id),
                     "connected": True,
                     "Data Type": "String",
-                    "Dictionary": str(data)
-                }
+                    "Dictionary": str(data),
+                },
             }
         }
         time.sleep(5)
@@ -63,6 +65,7 @@ def publish_thread(data):
     # Start the websocket connection
     ws.run_forever()  # Remove dispatcher parameter as it's not necessary anymore
 
+
 def on_error(ws, error):
     print(error)
 
@@ -73,6 +76,7 @@ def on_close(ws, close_status_code, close_msg):
 
 def on_open(ws):
     print("### Connection established ###")
+
 
 def send_data(data):
     # Start a new thread to publish the data
